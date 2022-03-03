32ade42d49afd995798b032469d21b23dfcc070a
EduardoReisUX <ducabruno09@hotmail.com>
2022-03-03 18:49:56 -0400

    test: add jest, testing library and its initial config

---
+++ src/tests/setupTests.ts
@@ -0,0 +1,8 @@
+// Polyfill "window.fetch" used in the React component.
+import "whatwg-fetch";
+
+// Extend Jest "expect" functionality with Testing Library assertions.
+import "@testing-library/jest-dom";
+
+// Thanks mswjs
+// https://github.com/mswjs/examples/blob/master/examples/with-jest/jest.setup.js
